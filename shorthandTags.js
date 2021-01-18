7c2d5f47935bea2c5247dcb46b1396a885a9914f
franklyT <48483484+franklyT@users.noreply.github.com>
2021-01-17 19:45:30 -0500

    Update shorthandTags.js

--- shorthandTags.js
+++ shorthandTags.js
@@ -23,3 +23,30 @@ function shorthandTags(str) {
 
   return str;
 }
+
+// Alternative Implementation
+/*
+function shorthandTags(str: string) {
+  const BLOCK_TEMPLATES = {
+    "<s": "<span",
+    "s>": "span>",
+    "<d": "<div",
+    "d>": "div>"
+  };
+
+  const MOD_TEMPLATES = {
+    "c|": "class=",
+    "h|": "href=",
+  };
+
+  _(BLOCK_TEMPLATES).each((blockVal, blockKey) => {
+    if (str.indexOf(blockKey) !== -1) str = str.split(blockKey).join(`${blockVal}`);
+  });
+
+  _(MOD_TEMPLATES).each((modVal, modKey) => {
+    if (str.indexOf(modKey) === -1) str = str.split(modKey).join(modVal);
+  });
+
+  return str;
+}
+*/
